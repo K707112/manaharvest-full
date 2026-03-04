de7d29da7c32491da950b94b8fb1a34258bb7a0b
royal12-aa <koteswararao1872005@gmail.com>
2026-03-04 22:01:58 +0530

    update project

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base:"/manaharvest-full/",
   plugins: [react()],
+  base:"/manaharvest-full/"
   server: {
     proxy: {
       '/api': {
